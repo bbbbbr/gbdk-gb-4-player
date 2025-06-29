1f2ca98e6760e46089162790554dd89fcdbcfc24
bbbbbr <bbbbbr@users.noreply.github.com>
2025-06-29 01:25:46 -0700

    - Support ring buffer on the main program side - Add some numeric debug readouts

--- src/title_screen.c
+++ src/title_screen.c
@@ -42,6 +42,12 @@ const uint8_t you_spr_x[] = {
     124u + (uint8_t)DEVICE_SPRITE_PX_OFFSET_X
 };
 
+#define RX_BUF_INITIAL_PACKET_IGNORE_COUNT   4   // Number of initial packets to ignore, works for TX size of 1. May need research for larger values
+uint8_t rx_packet_ignore_count;
+
+uint16_t rx_track[PLAYER_NUM_MAX][4];
+uint16_t tx_track[4];
+
 
 static void title_screen_init(void) {
 
@@ -56,6 +62,15 @@ static void title_screen_init(void) {
     set_sprite_data(SPR_YOU_ARROW_SPR_TILES_START, you_arrow_spr_TILE_COUNT, you_arrow_spr_tiles);
     set_sprite_data(SPR_SNAKE_TILES_START, snake_tiles_spr_TILE_COUNT, snake_tiles_spr_tiles);
     hide_sprites_range(0, MAX_HARDWARE_SPRITES);
+
+    rx_packet_ignore_count = RX_BUF_INITIAL_PACKET_IGNORE_COUNT;
+
+
+    for (uint8_t p = 0; p < PLAYER_NUM_MAX; p++) {
+        rx_track[p][0] = rx_track[p][1] = rx_track[p][2] = rx_track[p][3] = 0;
+        tx_track[p] = 0;
+    }
+
 }
 
 
@@ -94,53 +109,93 @@ static void update_connection_display(void) {
 #define DISPLAY_BUF_SZ (_4P_XFER_RX_SZ * 3)
 uint8_t display_buf[DISPLAY_BUF_SZ];
 
-static void handle_player_data(void) {
+static void dump_serial_stats(void) {
 
-    static uint8_t packets_ready;
+    uint8_t c;
+    uint8_t * p_display_buf;
+    // Display hex readout for all players rx and tx data
+
+    for (uint8_t p = 0; p < PLAYER_NUM_MAX; p++) {
+        p_display_buf = display_buf;
+        for (c = 0; c < 4; c++) {
+            // Print hex bytes separated by spaces
+            uint8_t value = rx_track[p][c];
+            *p_display_buf++ = (value >> 4) + BG_FONT_NUMS_TILES_START;
+            *p_display_buf++ = (value & 0x0F) + BG_FONT_NUMS_TILES_START;
+            *p_display_buf++ = BLANK_TILE;
+        }
+        uint8_t x = (WHICH_PLAYER_AM_I() == (p + 1)) ? 1 : 0;
+        set_bkg_tiles(x, p + 2, DISPLAY_BUF_SZ, 1, display_buf);
+    }
+
+    p_display_buf = display_buf;
+    for (c = 0; c < 4; c++) {
+        // Print hex bytes separated by spaces
+        uint8_t value = tx_track[c];
+        *p_display_buf++ = (value >> 4) + BG_FONT_NUMS_TILES_START;
+        *p_display_buf++ = (value & 0x0F) + BG_FONT_NUMS_TILES_START;
+        *p_display_buf++ = BLANK_TILE;
+    }
+    set_bkg_tiles(0,8, DISPLAY_BUF_SZ, 1, display_buf);
+
+}
+
+
+static void display_player_data(void) {
+
+    static uint8_t c;
+
+    // Display hex readout for all players
+    uint8_t * p_display_buf = display_buf;
+    for (c = 0; c < _4P_XFER_RX_SZ; c++) {
+
+        // Print hex bytes separated by spaces
+        uint8_t value = _4p_rx_buf_READ_ptr[c];
+        *p_display_buf++ = (value >> 4) + BG_FONT_NUMS_TILES_START;
+        *p_display_buf++ = (value & 0x0F) + BG_FONT_NUMS_TILES_START;
+        *p_display_buf++ = BLANK_TILE;
+    }
+    set_bkg_tiles(0,0, DISPLAY_BUF_SZ, 1, display_buf);
+
+    // Move a sprite for all players
+    uint8_t player_id_bit = _4P_PLAYER_1;
+
+    for (c = 0; c < _4P_XFER_RX_SZ; c++) {
+        if (IS_PLAYER_CONNECTED(player_id_bit)) {
+
+            uint8_t value = _4p_rx_buf_READ_ptr[c];
+
+            int8_t x_mv = 0;
+            if      (value & J_LEFT)  { x_mv = -1; rx_track[c][0]++; }
+            else if (value & J_RIGHT) { x_mv = 1;  rx_track[c][1]++; }
+
+            int8_t y_mv = 0;
+            if      (value & J_UP)   { y_mv = -1; rx_track[c][2]++; }
+            else if (value & J_DOWN) { y_mv = 1;  rx_track[c][3]++; }
+
+            scroll_sprite(c, x_mv, y_mv);            
+        }
+        player_id_bit <<= 1;
+    }
+}
+
+
+static void handle_player_data(void) {
 
     #ifdef DISPLAY_USE_SIO_DATA_DURATION_IN_BGP
         BGP_REG = ~BGP_REG;
     #endif
 
     // Read number of packets ready and cache it locally (it may change during processing)
-    packets_ready = four_player_rx_buf_get_num_packets_ready();
+    uint8_t packets_ready = four_player_rx_buf_get_num_packets_ready();
 
-    static uint8_t c;
+    for (uint8_t packet = 0; packet < packets_ready; packet++) {
 
-    for (uint8_t packet = 0; packet <= packets_ready; packet++) {
-
-        // Display hex readout for all players
-        uint8_t * p_display_buf = display_buf;
-        for (c = 0; c < _4P_XFER_RX_SZ; c++) {
-
-            // Print hex bytes separated by spaces
-            uint8_t value = _4p_rx_buf_READ_ptr[c];
-            *p_display_buf++ = (value >> 4) + BG_FONT_NUMS_TILES_START;
-            *p_display_buf++ = (value & 0x0F) + BG_FONT_NUMS_TILES_START;
-            *p_display_buf++ = BLANK_TILE;
-        }
-        set_bkg_tiles(0,0, DISPLAY_BUF_SZ, 1, display_buf);
-
-        // Move a sprite for all players
-        uint8_t player_id_bit = _4P_PLAYER_1;
-
-        for (c = 0; c < _4P_XFER_RX_SZ; c++) {
-            if (IS_PLAYER_CONNECTED(player_id_bit)) {
-
-                uint8_t value = _4p_rx_buf_READ_ptr[c];
-
-                int8_t x_mv = 0;
-                if      (value & J_LEFT)  x_mv = -1;
-                else if (value & J_RIGHT) x_mv = 1;
-
-                int8_t y_mv = 0;
-                if      (value & J_UP)   y_mv = -1;
-                else if (value & J_DOWN) y_mv = 1;
-
-                scroll_sprite(c, x_mv, y_mv);
-            }
-            player_id_bit <<= 1;
-        }
+        // Skip initial packets if requested
+        if (rx_packet_ignore_count)
+            rx_packet_ignore_count--;
+        else
+            display_player_data();
 
         // Move to next packet RX Bytes                
         _4p_rx_buf_packet_increment_read_ptr();
@@ -148,6 +203,9 @@ static void handle_player_data(void) {
 
     // free up the rx buf space now that it's done being used
     four_player_rx_buf_remove_n_packets(packets_ready);
+
+
+    dump_serial_stats();
 
     #ifdef DISPLAY_USE_SIO_DATA_DURATION_IN_BGP
         BGP_REG = ~BGP_REG;
@@ -216,14 +274,28 @@ void title_screen_run(void){
         }
         else if (GET_CURRENT_MODE() == _4P_STATE_XFER) {
             if (next_xfer_mode_redraw == true) {
+
+                rx_packet_ignore_count = RX_BUF_INITIAL_PACKET_IGNORE_COUNT;     // Number of initial packets to ignore
                 handle_game_screen_init();
+
                 next_ping_mode_redraw = true;
                 next_xfer_mode_redraw = false;
             }
 
             // Load TX data for next frame first
-            four_player_set_xfer_data(0x80u | (keys & J_DPAD));  // TODO: maybe lightweight checksum or etc on transmitted and received data
-            // four_player_set_xfer_data(0x80u | WHICH_PLAYER_AM_I());
+            // Only send data when there is a discrete event
+            uint8_t dpad_ticked = GET_KEYS_TICKED(J_DPAD);
+            if (dpad_ticked != 0u) {
+
+                if      (dpad_ticked & J_LEFT)  { tx_track[0]++; }
+                else if (dpad_ticked & J_RIGHT) { tx_track[1]++; }
+
+                if      (dpad_ticked & J_UP)   { tx_track[2]++; }
+                else if (dpad_ticked & J_DOWN) { tx_track[3]++; }
+
+                four_player_set_xfer_data(0x80u | dpad_ticked);  // TODO: maybe lightweight checksum or etc on transmitted and received data
+                // four_player_set_xfer_data(0x80u | WHICH_PLAYER_AM_I());
+            }
 
             if (IS_PLAYER_DATA_READY()) {
                 handle_player_data();
